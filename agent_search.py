52533e315500990062a70ff042d33c113346ac61
seolvov <hhhseolah@gmail.com>
2025-04-24 00:47:14 +0900

    llm, agent 구성 및 테스트

--- agent_search.py
+++ agent_search.py
@@ -1,6 +1,9 @@
-from langchain.tools import DuckDuckGoSearchRun
+import warnings
+warnings.filterwarnings("ignore") # 경고 숨기기
+
+from langchain_community.tools import DuckDuckGoSearchRun
 from langchain.agents import initialize_agent, AgentType, LLMSingleActionAgent
-from langchain.llms import HuggingFacePipeline
+from langchain_huggingface.llms import HuggingFacePipeline
 from transformers import pipeline
 
 # 1. 검색 툴
@@ -8,11 +11,13 @@ search = DuckDuckGoSearchRun()
 
 # 2. LLM 모델
 generator = pipeline(
-    "text-generation",
-    model="tiiuae/falcon-7b-instruct",
-    max_new_tokens=512,
+    "text2text-generation",
+    model="google/flan-t5-small", # "tiiuae/falcon-7b-instruct"이 너무 커서 터짐
+    device=-1, # CPU 강제 지정
+    max_new_tokens=256,
     do_sample=True,
-    temperature=0.7
+    temperature=0.5,
+    truncation=True
 )
 llm = HuggingFacePipeline(pipeline=generator)
 
@@ -21,7 +26,10 @@ agent = initialize_agent(
     tools=[search],
     llm=llm,
     agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
-    verbose=True
+    verbose=False, #verbose=Ture 일 경우 Agent 행동 로그 출력
+    handle_parsing_errors=True,
+    max_iterations=5, #반복제한
+    max_execution_time=60 #실행시간제한(60초)
 )
 
 # 4. 테스트
